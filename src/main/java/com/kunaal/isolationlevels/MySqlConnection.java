92f2d4d05ab639047fefa76ff34a7dc25b19579a
Oleksii Morenets <oleksii.morenets@gmail.com>
2024-04-25 12:02:05 +0200

    generate() method replaced

--- src/main/java/com/kunaal/isolationlevels/MySqlConnection.java
+++ src/main/java/com/kunaal/isolationlevels/MySqlConnection.java
@@ -8,32 +8,33 @@ import java.sql.SQLException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class MySqlConnection {
 
-	private static final String url = "jdbc:mysql://localhost:3306?serverTimezone=UTC";
-	private static final String user = "root";
-	private static final String password = "root";
+    private static final String url = "jdbc:mysql://localhost:3306?serverTimezone=UTC";
+    private static final String user = "root";
+    private static final String password = "root";
 
-	private MySqlConnection() {}
+    private MySqlConnection() {
+    }
 
-	public static Connection getConnection() {
-		try {
-			return DriverManager.getConnection(url, user, password);
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
-		}
-	}
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	public static void main(String[] args) throws SQLException, InterruptedException {
+    public static void main(String[] args) throws SQLException, InterruptedException {
 
-		ExecutorService executorService = Executors.newFixedThreadPool(10);
-		IntStream.rangeClosed(1, 10)
-				.mapToObj(value -> new Reader(MySqlConnection.getConnection()))
-				.forEach(executorService::execute);
-		executorService.shutdown();
-		executorService.awaitTermination(2, TimeUnit.SECONDS);
-		MySqlConnection.getConnection().close();
-	}
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        Stream.generate(() -> new Reader(MySqlConnection.getConnection()))
+				.limit(10)
+                .forEach(executorService::execute);
+        executorService.shutdown();
+        executorService.awaitTermination(2, TimeUnit.SECONDS);
+        MySqlConnection.getConnection().close();
+    }
 }
