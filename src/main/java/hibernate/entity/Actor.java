bb16d1149c6e71dd3f476016a886bf4cb7220992
Oleksii Morenets <oleksii.morenets@gmail.com>
2024-06-26 14:14:11 +0200

    hibernate Session methods examples

---
+++ src/main/java/hibernate/entity/Actor.java
@@ -0,0 +1,27 @@
+package hibernate.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class Actor {
+
+    @Id
+    @Column(name = "actor_id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "last_update")
+    private LocalDateTime lastUpdate;
+}
---
+++ src/main/java/hibernate/entity/Film.java
@@ -0,0 +1,15 @@
+package hibernate.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Film {
+
+    @Id
+    @Column(name = "film_id")
+    private int id;
+}
